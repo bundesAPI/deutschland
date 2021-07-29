65b83c442eac7e47b122916b2b8a4d3423a21860
Lilith Wittmann <mail@lilithwittmann.de>
2021-07-29 18:22:22 +0200

    add black

--- deutschland/bundesanzeiger/model.py
+++ deutschland/bundesanzeiger/model.py
@@ -5,10 +5,11 @@ import tensorflow.keras.backend as K
 import numpy as np
 from PIL import Image
 
-ALPHABET = list('abcdefghijklmnopqrstuvwxyz0123456789')
+ALPHABET = list("abcdefghijklmnopqrstuvwxyz0123456789")
+
 
 def load_image_arr(fp):
-    image = Image.open(fp).convert('L')
+    image = Image.open(fp).convert("L")
     image = np.array(image)
     image = image / 255 * 2
     image = image - 1
@@ -17,21 +18,22 @@ def load_image_arr(fp):
 
 def character_indexes_to_str(character_indexes):
     characters = np.array(ALPHABET)[character_indexes]
-    return ''.join(list(characters)).upper()
+    return "".join(list(characters)).upper()
 
 
 def prediction_to_str(label):
     character_indexes = np.argmax(label, axis=1)
     return character_indexes_to_str(character_indexes)
 
+
 def my_accuracy(y_true, y_pred):
     return K.cast(
-        K.all(
-            K.equal(K.argmax(y_true, axis=2), K.argmax(y_pred, axis=2)),
-            axis=1),
-        K.floatx())
+        K.all(K.equal(K.argmax(y_true, axis=2), K.argmax(y_pred, axis=2)), axis=1),
+        K.floatx(),
+    )
+
 
 def load_model():
-    return keras.models.load_model("assets/model.h5", custom_objects={'my_accuracy': my_accuracy})
-
-
+    return keras.models.load_model(
+        "assets/model.h5", custom_objects={"my_accuracy": my_accuracy}
+    )

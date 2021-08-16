e85a05937fc379231cd78cef899895ce1774990b
Lukas Panni <lukas.panni@outlook.de>
2021-08-16 16:39:39 +0200

    Fix: rearranged imports to remove circular import

--- deutschland/__init__.py
+++ deutschland/__init__.py
@@ -1,6 +1,7 @@
-from .geo import Geo
-from .bundesanzeiger.bundesanzeiger import Bundesanzeiger
-from .handelsregister.handelsregister import Handelsregister
 from .config import Config
 
 module_config = Config()
+
+from .geo import Geo
+from .bundesanzeiger.bundesanzeiger import Bundesanzeiger
+from .handelsregister.handelsregister import Handelsregister

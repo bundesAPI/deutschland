86223f8564cafe349f1d3333f6c4337fe8c02eb8
wirthual <wirthra@gmail.com>
2022-07-28 21:55:12 -0700

    make test names consitent. Black format geo test

--- tests/geo_test.py
+++ tests/geo_test.py
@@ -4,7 +4,9 @@ from deutschland.geo import Geo
 def test_basic_query():
     geo = Geo()
     # top_right and bottom_left coordinates
-    data = geo.fetch([52.530116236589244, 13.426532801586827],
-                     [52.50876180448243, 13.359631043007212])
+    data = geo.fetch(
+        [52.530116236589244, 13.426532801586827],
+        [52.50876180448243, 13.359631043007212],
+    )
 
     assert len(data.keys()) > 0

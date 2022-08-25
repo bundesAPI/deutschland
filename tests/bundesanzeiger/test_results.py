5153c23769c07fdec1b2badf90136100545d2144
wirthual <wirthra@gmail.com>
2022-08-24 20:44:03 -0700

    black formated

--- tests/bundesanzeiger/test_results.py
+++ tests/bundesanzeiger/test_results.py
@@ -1,5 +1,6 @@
 from deutschland.bundesanzeiger import Bundesanzeiger
 
+
 def test_results_not_empty():
     ba = Bundesanzeiger()
     reports = ba.get_reports("Deutsches Zentrum für Luft- und Raumfahrt")

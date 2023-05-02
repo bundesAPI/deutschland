821aebea0fb1534019226082ab00caec318b1787
wirthual <wirthra@gmail.com>
2023-05-02 08:45:56 -0700

    removed test

--- tests/presseportal_test.py
+++
@@ -1,9 +0,0 @@
-from deutschland.presseportal import Company, Entity, PresseportalApi, Story
-
-presseportal = PresseportalApi("YOUR_KEY_HERE")
-
-try:
-    stories = presseportal.get_stories()
-except Exception as e:
-    print(str(e))
-    assert str(e) == "The API returned error code 101 (authentification failed)."

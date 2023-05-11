019897eac29b778ff2825c6e614586592e00ffe5
wirthual <wirthra@gmail.com>
2023-05-11 08:55:06 -0700

    add isort as dev dependency.

--- src/deutschland/geo.py
+++ src/deutschland/geo.py
@@ -20,14 +20,14 @@ class Geo:
 
     def __deg2num(self, lat_deg, lon_deg, zoom):
         lat_rad = math.radians(lat_deg)
-        n = 2.0**zoom
+        n = 2.0 ** zoom
         xtile = int((lon_deg + 180.0) / 360.0 * n)
         ytile = int((1.0 - math.asinh(math.tan(lat_rad)) / math.pi) / 2.0 * n)
         return (xtile, ytile)
 
     # taken from https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Lon..2Flat._to_tile_numbers_2
     def __num2deg(self, xtile, ytile, zoom):
-        n = 2.0**zoom
+        n = 2.0 ** zoom
         lon_deg = xtile / n * 360.0 - 180.0
         lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * ytile / n)))
         lat_deg = math.degrees(lat_rad)

ce432f2072f4ed083d9f5f1dc1a655d1a45c055c
Maurice Atrops <matrops@cologne-intelligence.de>
2023-11-20 15:16:02 +0100

    Add unit tests for Bundesanzeiger.__get_response

---
+++ tests/bundesanzeiger/test_connection.py
@@ -0,0 +1,18 @@
+import pytest
+from requests import Response
+from deutschland.bundesanzeiger import Bundesanzeiger
+
+
+def test_exception_on_invalid_http_code():
+    ba = Bundesanzeiger()
+    with pytest.raises(ConnectionError):
+        ba._Bundesanzeiger__get_response("https://httpstat.us/500")
+        ba._Bundesanzeiger__get_response("https://httpstat.us/503")
+        ba._Bundesanzeiger__get_response("https://httpstat.us/404")
+
+
+def test_get_response():
+    ba = Bundesanzeiger()
+    assert (
+        ba._Bundesanzeiger__get_response("https://httpstat.us/200").status_code == 200
+    )

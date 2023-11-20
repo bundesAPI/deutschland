73cacbd70f0d1b76c179e11f9aece4d474712960
wirthual <wirthra@gmail.com>
2023-11-20 08:30:19 -0800

    Update test_connection.py
    Try different httpstatus side since the current one seems to have trouble.

--- tests/bundesanzeiger/test_connection.py
+++ tests/bundesanzeiger/test_connection.py
@@ -5,13 +5,13 @@ from deutschland.bundesanzeiger import Bundesanzeiger
 def test_exception_on_invalid_http_code():
     ba = Bundesanzeiger()
     with pytest.raises(ConnectionError):
-        ba._Bundesanzeiger__get_response("https://httpstat.us/500")
-        ba._Bundesanzeiger__get_response("https://httpstat.us/503")
-        ba._Bundesanzeiger__get_response("https://httpstat.us/404")
+        ba._Bundesanzeiger__get_response("https://mock.httpstatus.io/500")
+        ba._Bundesanzeiger__get_response("https://mock.httpstatus.io/503")
+        ba._Bundesanzeiger__get_response("https://mock.httpstatus.io/404")
 
 
 def test_get_response():
     ba = Bundesanzeiger()
     assert (
-        ba._Bundesanzeiger__get_response("https://httpstat.us/200").status_code == 200
+        ba._Bundesanzeiger__get_response("https://mock.httpstatus.io/200").status_code == 200
     )
